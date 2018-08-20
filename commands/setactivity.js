86ad077fe22f3d68e3f90ad387896ed09aed51b9
Deres54 <40034875+Deres54@users.noreply.github.com>
2018-08-20 22:21:11 +0300

    kek

--- commands/setactivity.js
+++ commands/setactivity.js
@@ -1,1 +1,18 @@
-//tut budet kod
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+let type = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Слушай, а ты уверен, что ты так можешь? :thinking:"); 
+
+if(!type) return message.channel.send("ДАГ ШО МНЕ ДЕЛАТЬ?! Играть (1), стримить (2), смотреть (3)");
+
+}
+
+module.exports.help = {
+
+name: "kick"
+
+}
+
