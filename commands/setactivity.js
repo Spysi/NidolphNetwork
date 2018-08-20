1dce8f01f0f78253bf21841442e8b548db2a030c
Deres54 <40034875+Deres54@users.noreply.github.com>
2018-08-20 22:26:25 +0300

    New

--- commands/setactivity.js
+++ commands/setactivity.js
@@ -2,11 +2,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-let type = args[0];
+let type = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+let what = args.slice(1).join(' ');
 
 if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Слушай, а ты уверен, что ты так можешь? :thinking:"); 
 
 if(!type) return message.channel.send("ДАГ ШО МНЕ ДЕЛАТЬ?! Играть (1), стримить (2), смотреть (3)");
+
+if(!what) return message.channel.send("Что дальше? :thinking:");
 
 }
 
@@ -15,4 +19,3 @@ module.exports.help = {
 name: "setactivity"
 
 }
-
