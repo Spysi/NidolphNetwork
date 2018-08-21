ba5e712d94226b2d4b12bc8d5b23aabcd94635ce
Vlad Ciphersky <admin@eclipsedev.cf>
2018-08-21 09:14:31 +0500

    Create phearting.js

---
+++ commands/phearting.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  const status = args.join(" ");
+  client.user.setPresence({ game: { name: status, type: 2 } }).catch();
+  message.channel.send(`:ok_hand:`);
+}
+
+module.exports.help = {
+  name: "phearting"
+}
