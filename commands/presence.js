a8ed0277e78834405863928cdadbb20a242951a1
Vlad Ciphersky <admin@eclipsedev.cf>
2018-08-21 09:04:17 +0500

    Фиксим #2

--- commands/presence.js
+++ commands/presence.js
@@ -1,16 +1,19 @@
 const Discord = require("discord.js");
+
 module.exports.run = async (bot, message, args) => {
   
-        const typee = args.slice(0).join(' ');  
+        const typee = args.join(' ');  
         const status = args.slice(1).join(' ');
         client.user.setPresence({ game: { name: status, type: typee } }).catch();
 
-        const embed = new Discord.RichEmbed()
+        const okkkkk = new Discord.RichEmbed()
             .setTitle('Статус изменен:')
             .setDescription(`Тип: ${typee} | Статус: ${status}`)
             .setFooter('NidolphNetwork');
-message.channel.send({embed});
+message.channel.send(okkkkk);
+  
 }
+
 module.exports.help = {
-  name: "setactivity"
+  name: "presence"
 }
