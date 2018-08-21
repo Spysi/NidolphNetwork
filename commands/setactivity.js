fa0c4c00454f176b135e59228b28bdc3d068d49e
Vlad Ciphersky <admin@eclipsedev.cf>
2018-08-21 08:59:26 +0500

    Фиксим

--- commands/setactivity.js
+++ commands/setactivity.js
@@ -1,21 +1,16 @@
 const Discord = require("discord.js");
-
 module.exports.run = async (bot, message, args) => {
+  
+        const typee = args.slice(0).join(' ');  
+        const status = args.slice(1).join(' ');
+        client.user.setPresence({ game: { name: status, type: typee } }).catch();
 
-let type1 = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-
-let what = args.slice(1).join(' ');
-
-if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Слушай, а ты уверен, что ты так можешь? :thinking:"); 
-
-if(!type1) return message.channel.send("ДАГ ШО МНЕ ДЕЛАТЬ?! Играть (1), стримить (2), смотреть (3)");
-
-if(!what) return message.channel.send("Что дальше? :thinking:");
-
-bot.user.setActivity(what, {type: type1}
-
+        const embed = new Discord.RichEmbed()
+            .setTitle('Статус изменен:')
+            .setDescription(`Тип: ${typee} | Статус: ${status}`)
+            .setFooter('NidolphNetwork');
+message.channel.send({embed});
+}
 module.exports.help = {
-
-name: "setactivity"
-
+  name: "setactivity"
 }
