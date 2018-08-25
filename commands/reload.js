eb64525ade80c9cea7c017fc64c7362e201fde64
Vlad Ciphersky <admin@eclipsedev.cf>
2018-08-25 16:28:11 +0500

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,13 +1,10 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-  if(message.author.id === "303872342457843722" || message.author.id === "178404926869733376") {
+  if(message.author.id !== "303872342457843722" || message.author.id !== "178404926869733376") return message.reply("У Вас недостаточно прав для совершения данного действия");
     if(!args || args.size < 1) return message.reply("Вы должны указать кодовое имя команды");
     delete require.cache[require.resolve(`./${args[0]}.js`)];
     message.channel.send(`Команда ${args[0]} перезагружена.`);
-  } else {
-    message.reply("У Вас недостаточно прав, для выполнения данной команды");
-  }
 };
 
 module.exports.help = {
