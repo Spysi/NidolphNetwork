39d540cff113694d222ca6b49f94a565de209fab
Andrey Karas <40034875+Deres54@users.noreply.github.com>
2018-08-28 20:25:54 +0700

    КеК в2

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if(!args[0]) return message.reply("Нет вопроса!");
     if(message.content.includes("пидор")){
-      if(message.content.includes("я") return message.reply("Да");
+      if(message.content.includes("я")) return message.reply("Да");
       else return message.reply("сам пидор");
     } 
     let answers = ["да", "нет", "отвечаю, да", "это все знают, да", "ТЫ ШО? Конечно нет", "ниииииииииэээээт", "возможно", "спросите позже", "перезвоните позже", "не знаю", "дайте подумать", "подожди, я погуглю ответ", "сложный вопрос", "ААААААААААА СЛОЖНА", "ОШИБКА СТОП 0x00000000ААААААА"];
