6bc37ccefa7382c9af6e9d4eba159037e9338dd4
Andrey Karas <40034875+Deres54@users.noreply.github.com>
2018-08-29 11:48:20 +0700

    Новая команда !setstatus

---
+++ commands/setstatus.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+ module.exports.run = async (bot, message, args) => {
+        if(!message.author.hasPermission(ADMINISTRATOR)) return message.reply("Ты это не умеешь. ни ломай бота.")
+        const typee = args[0];
+        const game = args.slice(2).join(' ');
+          if(typee == "PLAYING"){
+            bot.user.setActivity(game, { type: 'PLAYING' });
+          }
+          else if(typee == "STREAMING"){
+            bot.user.setActivity(game, { type: 'STREAMING' });
+          }
+          else if(typee == "LISTENING"){
+            bot.user.setActivity(game, { type: 'LISTENING' });
+          }
+          else if(typee == "WATCHING"){
+            bot.user.setActivity(game, { type: 'WATCHING' });
+          }
+          else{
+            message.reply("Нет такого статуса!");
+          }
+         const okkkkk = new Discord.RichEmbed()
+            .setTitle('Статус изменен:')
+            .setDescription(`Тип: ${typee} | Статус: ${status}`)
+            .setFooter('NidolphNetwork');
+          message.channel.send(okkkkk);
+
+}
+ module.exports.help = {
+  name: "setstatus"
+}
