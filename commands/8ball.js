12e5274dbc3859b96c274828fe6742b51e4488b5
Andrey Karas <40034875+Deres54@users.noreply.github.com>
2018-08-29 11:21:03 +0700

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,12 +1,15 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if(!args[0]) return message.reply("Нет вопроса!");
-    if(message.content.includes("пидор")){
-      if(message.content.includes("я")) return message.reply("Да");
+    if(!args[0]) return message.reply("нет вопроса!");
+    if(message.content.includes("пидор") || message.content.includes("пид0р")){
+      if(message.content.includes("я") || message.content.includes("Я")) return message.reply("да");
       else return message.reply("сам пидор");
-    } 
-    let answers = ["да", "нет", "отвечаю, да", "это все знают, да", "ТЫ ШО? Конечно нет", "ниииииииииэээээт", "возможно", "спросите позже", "перезвоните позже", "не знаю", "дайте подумать", "подожди, я погуглю ответ", "сложный вопрос", "ААААААААААА СЛОЖНА", "ОШИБКА СТОП 0x00000000ААААААА"];
+    }
+    let answers = ["да", "отвечаю, да", "это все знают, да",
+    "ТЫ ШО? Конечно нет", "ниииииииииэээээт", "нет",
+    "возможно", "спросите позже", "перезвоните позже", "не знаю", "дайте подумать", "подожди, я погуглю ответ",
+    "сложный вопрос", "ААААААААААА СЛОЖНА", "ОШИБКА СТОП 0x00000000ААААААА"];
     let result = Math.floor((Math.random() * answers.length));
 
     message.reply(`${answers[result]}`);
